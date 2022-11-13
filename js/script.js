76e6d22e1be4d46db0842fc9654de38d5f00b99c
mjlsebastian <mjsebastian27@gmail.com>
2022-11-13 16:13:50 +0800

    second-commit

--- js/script.js
+++ js/script.js
@@ -1,38 +1,42 @@
-var you;
-var yourScore = 0;
-var opponent;
-var opponentScore = 0;
+let you;
+let yourScore = 0;
+let opponent;
+let opponentScore = 0;
 
-var choices = ["rock", "paper", "scissors"]
+let choices = ["rock", "paper", "scissors"]
+    
 
 window.onload = function() {
     for (let i = 0; i < 3;  i++) {
         // <img id=rock" src="rock.png">
         let choice = document.createElement("img");
         choice.id = choices[i];
-        choice.src = choices[i] + ".png";
+        choice.src = choices[i] + ".png" ;
         choice.addEventListener("click", selectChoice);
         document.getElementById("choices").append(choice);
+        
     }
  }
 
  function selectChoice() {
     you = this.id;
-    document.getElementById("your-choice").src = you + ".png";
+    document.getElementById("yourChoice").src = you + ".jpg";
 
     //random for opponent
     opponent = choices[Math.floor(Math.random() * 3)]; // 0- .99999 * 3 = 0-2.99999
-    document.getElementById("opponent-choice").src = opponent + ".png";
+    document.getElementById("opponentChoice").src = opponent + ".png";
+    
 
     //check for winner
     if (you == opponent) {
         yourScore += 1;
-        opponentScore += 1;
+        opponentScore += 1; 
     }
     else {
         if (you == "rock") {
             if (opponent == "scissors") {
-                yourScore += 1;
+                yourScore += 1; 
+                
             }
             else if (opponent == "paper") {
                 opponentScore += 1;
@@ -41,27 +45,36 @@ window.onload = function() {
         else if (you == "scissors") {
             if (opponent == "paper") {
                 yourScore += 1;
+                
             }else if (opponent == "rock") {
                 opponentScore += 1;
             }
         }
         else if (you == "paper") {
             if (opponent == "rock") {
-                yourScore += 1;
+                yourScore += 1; 
+                
             }else if (opponent == "scissors") {
                 opponentScore += 1;
             }
         }
+        // if (you == opponent) {
+        //     yourScore  < 3;
+        //     alert("You Win!")
+        // }
+        // else if (yourScore > 3) {
+        //     alert("You Lose!")
+        // } 
+        // resetButton.addEventListener("click", resetBoard);
 
-        
     }
 
-    function checkWinner(){
-        for (let r = 0 );  
-    }
+
+    document.getElementById("yourScore").innerText = yourScore;
+    document.getElementById("opponentScore").innerText = opponentScore;
+    resetButton.addEventListener("click", resetBoard);
 
 
-    document.getElementById("your-score").innerText = yourScore;
-    document.getElementById("opponent-score").innerText = opponentScore;
+
     
  }
